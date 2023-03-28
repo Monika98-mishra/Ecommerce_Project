79117c834ac35f09abff60953d8faa07dc58b5c9
Monika Mishra <monikamishra7198@gmail.com>
2023-03-28 16:55:21 +0530

    Adding pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import { Button } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import AddProduct from './AddProduct';
+import UpdateProduct from './UpdateProduct';
 
 function App() {
   return (
     <div className="App">
-      Hello Monika,welcome
+       <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="login" element={<Login />}> </Route>
+          <Route path="register" element={<Register />}> </Route>
+          <Route path="update" element={<UpdateProduct />}> </Route>
+          <Route path="add" element={<AddProduct />}> </Route>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/Register.js
@@ -0,0 +1,15 @@
+function Register(){
+    return(
+        <div className="col-sm-6 offset-sm-3">
+            <h1>User Sign Up</h1>
+            <input type="text" className="form-control"/>
+            <br/>
+            <input type="text" className="form-control"/>
+            <br/>
+            <input type="text" className="form-control"/>
+            <br/>
+            <button className="btn btn-primary">SignUp</button>
+        </div>
+    )
+}
+export default Register;
