5f640a7ec1477ac3635e164fd2c429f9d623ab32
Monika Mishra <monikamishra7198@gmail.com>
2023-04-04 11:54:12 +0530

    added files

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -1,10 +1,40 @@
 import Header from './Header';
+import { useState } from 'react';
 function AddProduct(){
+    const [name,setName]=useState("");
+    const [file,setFile]=useState("");
+    const [price,setPrice]=useState("");
+    const [description,setDescription]=useState("");
+    const addProduct=async()=>{
+        console.log(name,file,price,description);
+        const formData = new FormData();
+        formData.append("file",file[0]);
+        formData.append("name",name);
+        formData.append("price",price);
+        formData.append("description",description);
+        let result=await fetch("http://localhost:3000/addproduct",{
+            method:'POST',
+            headers:{
+                "Content-Type":"application/json",
+                "Accept":"application/json"
+            },
+            body:JSON.stringify(formData)
+        });
+        result=await result.json();
+        console.log("result",result);
+    }
     return(
-        <div>
-             <Header/>
-            <h1>AddProduct Page</h1>
+        <>
+          <Header/>
+        <div className='col-sm-6 offset-sm-3'>
+           <br/>
+            <input type="text" onChange={(e)=>setName(e.target.value)} className='form-control' placeholder='name'/><br/>
+            <input type="file"  onChange={(e)=>setFile(e.target.files[0])} className='form-control' placeholder='file'/><br/>
+            <input type="text"  onChange={(e)=>setPrice(e.target.value)} className='form-control' placeholder='price'/><br/>
+            <input type="text"  onChange={(e)=>setDescription(e.target.value)} className='form-control' placeholder='description'/><br/>
+            <button className='btn btn-primary' onClick={addProduct}>AddProduct</button>
         </div>
+        </>
     )
 }
 export default AddProduct;
--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,8 @@ import { useState,useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import Header from './Header';
 function Login(){
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
     useEffect(()=>{
         if(localStorage.getItem('user-info'))
         {
@@ -9,12 +11,31 @@ function Login(){
         }
     },[])
     const navigate = useNavigate();
+    const login=async ()=>{
+        let item={email,password};
+        let result= await fetch("http://localhost:3000/login",{
+            method:'POST',
+            headers:{
+                "Content-Type":"application/json",
+                "Accept":"application/json"
+            },
+            body:JSON.stringify(item)
+        });
+        result=await result.json();
+        localStorage.setItem('user-info',JSON.stringify(result));
+        navigate('/add');
+    }
     
     return(
-        <div>
-             <Header />
+        <>
+        <Header />
             <h1>Login Page</h1>
+           <div className="col-sm-6 offset-sm-3"> 
+            <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)} className="form-control" placeholder="Email"/><br/>
+            <input type="text" value={password} onChange={(e)=>setPassword(e.target.value)}className="form-control" placeholder="Password"/><br/>
+            <button onClick={login} className="btn btn-primary">Login</button>
         </div>
+        </>
     )
 }
 export default Login;
